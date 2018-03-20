542e7c98dca6c66eb70ad1ddd5a0ea31359f06e2
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2018-03-19 23:08:17 -0700

    use ember qunit codemod

--- tests/integration/components/drum-kit-key-test.js
+++ tests/integration/components/drum-kit-key-test.js
@@ -1,25 +1,27 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('drum-kit-key', 'Integration | Component | drum kit key', {
-  integration: true
-});
+module('Integration | Component | drum kit key', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
+  test('it renders', async function(assert) {
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{drum-kit-key}}`);
+    await render(hbs`{{drum-kit-key}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#drum-kit-key}}
-      template block text
-    {{/drum-kit-key}}
-  `);
+    // Template block usage:
+    await render(hbs`
+      {{#drum-kit-key}}
+        template block text
+      {{/drum-kit-key}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(this.$().text().trim(), 'template block text');
+  });
 });
--- tests/unit/controllers/drum-kit-test.js
+++ tests/unit/controllers/drum-kit-test.js
@@ -1,12 +1,12 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('controller:drum-kit', 'Unit | Controller | drum kit', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
-});
+module('Unit | Controller | drum kit', function(hooks) {
+  setupTest(hooks);
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:drum-kit');
+    assert.ok(controller);
+  });
 });
